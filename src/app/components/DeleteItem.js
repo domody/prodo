1e2c9dfb79b6346dc14ed5502d133a739812706c
Dom Ody <dom20.ody07@gmail.com>
2024-06-04 21:37:44 +0200

    Basic Item implementer (test) pls async it

---
+++ src/app/components/DeleteItem.js
@@ -0,0 +1,27 @@
+// app/components/DeleteItem.js
+import db from "../../../lib/firestore";
+import { doc, deleteDoc } from "firebase/firestore";
+
+const DeleteItem = ({ id }) => {
+  const handleDelete = async () => {
+    const itemRef = doc(db, "items", id);
+    try {
+      await deleteDoc(itemRef);
+      alert("Item deleted successfully");
+    } catch (error) {
+      console.error("Error deleting document: ", error);
+      alert("Error deleting item");
+    }
+  };
+
+  return (
+    <button
+      onClick={handleDelete}
+      className="rounded border bg-red-400 p-1 text-white"
+    >
+      Delete Item
+    </button>
+  );
+};
+
+export default DeleteItem;
--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,54 @@
-import Footer from "./components/footer";
-import Navbar from "./components/navbar";
+// import Footer from "./components/footer";
+// import Navbar from "./components/navbar";
+
+// export default function Home() {
+//   return (
+//     <main className="flex w-screen flex-col items-center justify-center text-balance">
+//       <div className="absolute top-0 -z-50 mx-auto aspect-[2/1] w-[36rem] rounded-b-full bg-[#181933] blur-[150px]"></div>
+//       {/* Page Start */}
+//       <Navbar />
+
+//       <div className="container mx-auto mt-24 flex flex-col items-center justify-start">
+//         <div className="flex flex-col items-center justify-start">
+//           <div className="flex items-center justify-center rounded-full border border-pink-200/30 bg-gradient-to-b from-pink-300/[0.02] to-pink-300/15 px-5 py-1.5 text-sm">
+//             <div className="bg-gradient-to-r from-purple-400 via-pink-400 to-orange-200 bg-clip-text text-transparent">
+//               New: Mobile v1.0
+//             </div>
+//           </div>
+//           <h1 className="mt-4 bg-gradient-to-b from-light to-[#bbbbbb] bg-clip-text text-center text-7xl font-semibold text-transparent">
+//             Seamless Collaboration for <br />
+//             Modern Teams
+//           </h1>
+//           <p className="mt-8 max-w-4xl text-center text-lg font-medium text-dark-200">
+//             Designed for efficiency and clarity, ProDo helps your team stay in
+//             sync and focused on what matters.
+//           </p>
+//         </div>
+
+//         <div className="blur-backdrop-sm relative mt-32 aspect-video w-full rounded-xl bg-slate-950"></div>
+
+//         <div className="flex flex-col items-center justify-start py-40">
+//           <h1 className="mt-4 bg-gradient-to-r from-light to-[#bbbbbb] bg-clip-text text-center text-6xl font-medium leading-relaxed text-transparent">
+//             Everything Your Team Needs in One Place
+//           </h1>
+//         </div>
+//       </div>
+//       <Footer />
+//     </main>
+//   );
+// }
+
+"use client";
+
+import AddItem from "./components/AddItem";
+import ListItems from "./components/ListItem"
 
 export default function Home() {
   return (
-    <main className="flex w-screen flex-col items-center justify-center text-balance">
-      <div className="absolute top-0 -z-50 mx-auto aspect-[2/1] w-[36rem] rounded-b-full bg-[#181933] blur-[150px]"></div>
-      {/* Page Start */}
-      <Navbar />
-
-      <div className="container mx-auto mt-24 flex flex-col items-center justify-start">
-        <div className="flex flex-col items-center justify-start">
-          <div className="flex items-center justify-center rounded-full border border-pink-200/30 bg-gradient-to-b from-pink-300/[0.02] to-pink-300/15 px-5 py-1.5 text-sm">
-            <div className="bg-gradient-to-r from-purple-400 via-pink-400 to-orange-200 bg-clip-text text-transparent">
-              New: Mobile v1.0
-            </div>
-          </div>
-          <h1 className="mt-4 bg-gradient-to-b from-light to-[#bbbbbb] bg-clip-text text-center text-7xl font-semibold text-transparent">
-            Seamless Collaboration for <br />
-            Modern Teams
-          </h1>
-          <p className="mt-8 max-w-4xl text-center text-lg font-medium text-dark-200">
-            Designed for efficiency and clarity, ProDo helps your team stay in
-            sync and focused on what matters.
-          </p>
-        </div>
-
-        <div className="blur-backdrop-sm relative mt-32 aspect-video w-full rounded-xl bg-slate-950"></div>
-
-        <div className="flex flex-col items-center justify-start py-40">
-          <h1 className="mt-4 bg-gradient-to-r from-light to-[#bbbbbb] bg-clip-text text-center text-6xl font-medium leading-relaxed text-transparent">
-            Everything Your Team Needs in One Place
-          </h1>
-        </div>
-      </div>
-      <Footer />
-    </main>
+    <div>
+      <h1>Welcome to My Next.js App</h1>
+      <AddItem />
+      <ListItems />
+    </div>
   );
 }
