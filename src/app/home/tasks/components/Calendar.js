6eb56d584a51dddbacc6ac998b3e9009db41c231
SpoonFish <matthewrisso2007@outlook.com>
2024-06-07 18:10:36 +0200

    day and month selection and clamps width to same height (7 grid cells) for every month so buttons do not move

--- src/app/home/tasks/components/Calendar.js
+++ src/app/home/tasks/components/Calendar.js
@@ -1,20 +1,13 @@
 import { useState } from "react";
-import {
-  X,
-  Tag,
-  CircleSlash,
-  CalendarIcon,
-  AlertCircle,
-  Info,
-} from "lucide-react";
+import { ChevronRight, ChevronLeft } from "lucide-react";
 import { Circle } from "react-feather";
 
 const Calendar = ({ visible, setCreateTaskVisibility }) => {
   const [date, setDate] = useState(new Date());
-  const days = Array.from(
-    { length: new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate() },
-    (_, i) => i + 1,
+  const [daysInCurrentMonth, setDaysInMonth] = useState(
+    new Date(date.getFullYear(), date.getMonth(), 0).getDate(),
   );
+  const days = Array.from({ length: daysInCurrentMonth }, (_, i) => i + 1);
 
   const [weekdayStartDay, setWeekdayStartDay] = useState(
     new Date(date.getFullYear(), date.getMonth(), 1).getDay() - 1,
@@ -36,10 +29,47 @@ const Calendar = ({ visible, setCreateTaskVisibility }) => {
   ];
 
   const dateMonth = months[date.getMonth()];
+  const dateYear = date.getFullYear();
   const dateDay = date.getDate();
+
+  const setDueDay = (day) => {
+    setDate(new Date(date.getFullYear(), date.getMonth(), day));
+  };
+
+  const increaseMonth = () => {
+    if (date.getMonth() == 13) {
+      setDate(new Date(date.getFullYear() + 1, 1, date.getDay()));
+    } else {
+      setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1));
+    }
+    setDaysInMonth(
+      new Date(date.getFullYear(), date.getMonth() + 2, 0).getDate(),
+    );
+  };
+
+  const decreaseMonth = () => {
+    if (date.getMonth() == 0) {
+      setDate(new Date(date.getFullYear() - 1, 11, date.getDay()));
+    } else {
+      setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1));
+    }
+    setDaysInMonth(new Date(date.getFullYear(), date.getMonth(), 0).getDate());
+  };
+
   return (
     <div className="flex w-full flex-col items-center justify-center rounded-lg border border-dark-500 p-4">
-      <div className="pb-2 text-2xl">{dateMonth}</div>
+      <div className="b-2 pb-1 font-semibold text-dark-300">{dateYear}</div>
+      <div className="flex pb-4">
+        <button onClick={() => decreaseMonth()}>
+          <ChevronLeft className="mx-2.5 h-8 w-8" />
+        </button>
+
+        <div className="w-32 text-center text-2xl">{dateMonth}</div>
+
+        <button onClick={() => increaseMonth()}>
+          <ChevronRight className="mx-2.5 h-8 w-8" />
+        </button>
+      </div>
       <div className="grid w-full grid-cols-7 gap-2">
         <div className="rounded-lg bg-dark-800 py-2 text-center text-lg">
           Mon
@@ -71,9 +101,13 @@ const Calendar = ({ visible, setCreateTaskVisibility }) => {
           <div
             key={day}
             className={`cursor-pointer rounded-lg py-5 text-center transition-all hover:bg-dark-800 ${dateDay === day ? "border-2 border-red-950" : "border-2 border-dark-800"}`}
+            onClick={() => setDueDay(day)}
           >
             {day}
           </div>
+        ))}
+        {[...Array(42 - daysInCurrentMonth - weekdayStartDay)].map((_) => (
+          <div className="py-[32px]"></div>
         ))}
       </div>
     </div>
